e3570a8ba6082dca333c38b59a2825d6af05257d
Fachri Fajar <101514724+fachrifajar@users.noreply.github.com>
2025-07-05 11:44:34 +0700

    feat: implement throttler handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,40 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UsersModule, EmailModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    UsersModule,
+    EmailModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
